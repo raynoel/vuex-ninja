22d27fab7d9d46003a41d151a08117d58d927268
ray <ray@ray.com>
2020-08-17 18:12:59 -0400

    Setting-up the store

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    products: [
+      { name: 'Banana Skin', price: 20 },
+      { name: 'Shiny Star', price: 40 },
+      { name: 'Green Shells', price: 60 },
+      { name: 'Red Shells', price: 80 },
+    ]
+  },
   mutations: {},
   actions: {},
   modules: {}
