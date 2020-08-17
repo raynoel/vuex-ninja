69baecb437b02d08582df90d97c42cd2d5e5a5d6
ray <ray@ray.com>
2020-08-17 17:42:20 -0400

    Page Vue initiale

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view v-bind:products="products" />
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        { name: 'Banana Skin', price: 20 },
+        { name: 'Shiny Star', price: 40 },
+        { name: 'Green Shells', price: 60 },
+        { name: 'Red Shells', price: 80 },
+      ]
+    }
+  }
+};
+</script>
+
+
+<style lang="scss">
+body{
+  font-family: Ubuntu;
+  color: #555;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
+</template>
+
+
+
+
+<script>
+import ProductListOne from '../components/ProductListOne.vue'
+import ProductListTwo from '../components/ProductListTwo.vue'
+
+export default {
+  name: "Home",
+  components: {
+    ProductListOne,
+    'product-list-two': ProductListTwo,
+  },
+  props: ['products'],
+
+};
+</script>
+
+
+
+
+<style lang="scss">
+</style>
